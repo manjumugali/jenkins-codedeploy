49ae311d98771783248cacec109885dac0b11bcd
Manjunath <35911822+manjumugali@users.noreply.github.com>
2019-04-27 13:57:30 +0530

    Update installDependecies.sh

--- scripts/installDependecies.sh
+++ scripts/installDependecies.sh
@@ -5,11 +5,10 @@ sudo apt-get update
 sudo pip3 install virtualenv
 virtualenv --python=python3 /home/ubuntu/venv
 source /home/ubuntu/venv/bin/activate
-cd /home/ubuntu/ChatApp-Django/
-pip3 install -r requirements.txt
+pip3 install -r /home/ubuntu/ChatApp-Django/requirements.txt
 pip3 install django bcrypt django-extensions
 sudo chown ubuntu:ubuntu /home/ubuntu/ChatApp-Django
-python3 manage.py collectstatic
+python3 /ChatApp-Django/manage.py collectstatic
 sudo rm -rf /etc/nginx/sites-available/ChatApp-Django
 sudo cp /home/ubuntu/ChatApp-Django/files/ChatApp-Django /etc/nginx/sites-available/
 sudo rm -rf /etc/nginx/sites-enabled/ChatApp-Django

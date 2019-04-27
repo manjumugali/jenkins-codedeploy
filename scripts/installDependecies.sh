2e3df4e82d27d96f7435f4fdbd6b42da763dec45
Manjunath <35911822+manjumugali@users.noreply.github.com>
2019-04-27 14:47:52 +0530

    Update installDependecies.sh

--- scripts/installDependecies.sh
+++ scripts/installDependecies.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-sudo apt-get update
 sudo apt-get install python3-pip python3-dev nginx git -y
-sudo apt-get update
-sudo pip3 install virtualenv
+sudo apt-get install virtualenv -y
 virtualenv --python=python3 /home/ubuntu/venv
 sudo chown ubuntu:ubuntu /home/ubuntu/ChatApp-Django
 source /home/ubuntu/venv/bin/activate
 pip3 install -r /home/ubuntu/ChatApp-Django/requirements.txt
-pip3 install django bcrypt django-extensions
-cd /home/ubuntu/ChatApp-Django
-python3 manage.py collectstatic
-sudo rm -rf /etc/nginx/sites-available/ChatApp-Django
+pip3 install django bcrypt django-extensions 
+sudo cp /home/ubuntu/ChatApp-Django/files/gunicorn.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+sudo unlink /etc/nginx/sites-enabled/*
 sudo cp /home/ubuntu/ChatApp-Django/files/ChatApp-Django /etc/nginx/sites-available/
-sudo rm -rf /etc/nginx/sites-enabled/ChatApp-Django
-sudo ln -s /etc/nginx/sites-available/ChatApp-Django /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/ChatApp-Django /etc/nginx/sites-enabled
 sudo nginx -t
+sudo rm /etc/nginx/sites-enabled/default
+sudo service nginx restart
